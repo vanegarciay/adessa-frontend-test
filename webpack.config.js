22cb11e39d45eec688249f12e4a4bb9eb3a6c785
Sebastian Real <sreal@falabella.cl>
2017-01-27 15:37:50 -0300

    Additional configuration for webpack
    - adding resolvers to override them
    
    - sourcemaps for dev and production
    
    PR: https://github.com/aether7/adessa-frontend-test/pull/2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 const WebpackBuildNotifier = require('webpack-build-notifier');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: isProduction ? 'hidden-source-map' : 'inline-source-map',
   entry: {
     main: './static/js/apps/main.jsx'
   },
@@ -26,6 +27,10 @@ module.exports = {
         ]
       }
     ]
+  },
+  resolve : {
+    extensions: ['.js', '.json', '.jsx'],
+    modules: ['node_modules'/* add additional folders right here */]
   },
   plugins: [
     new WebpackBuildNotifier()
